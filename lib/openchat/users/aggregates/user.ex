a818ace90d2e265461c27661f35e243f19cac0de
Daniele Megna <daniele.megna@casavo.com>
2020-08-31 14:40:24 +0200

    User aggregate emits a UserRegistered event on RegisterUser command
    ... and handle the event to mutate the state (still fake now)

--- lib/openchat/users/aggregates/user.ex
+++ lib/openchat/users/aggregates/user.ex
@@ -2,8 +2,16 @@ defmodule Openchat.Users.Aggregates.User do
   defstruct [:id, :username, :password, :about]
   
   alias Openchat.Users.Commands.RegisterUser
+  alias Openchat.Users.Events.UserRegistered
 
   def execute(%__MODULE__{} = _state, %RegisterUser{} = _command) do
-    []
+    %UserRegistered{id: "fixme", username: "fixme", password: "fixme", about: "fixme"}
   end
+
+  # state mutators
+
+  def apply(%__MODULE__{} = state, %UserRegistered{} = _event) do
+    state
+  end
+
 end
