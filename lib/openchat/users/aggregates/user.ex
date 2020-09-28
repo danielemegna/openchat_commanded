4257f0b00ed9e9721fddf416c26396266e950824
Daniele Megna <daniele.megna@casavo.com>
2020-09-28 09:19:22 +0200

    Refactor in Aggregates.Users

--- lib/openchat/users/aggregates/user.ex
+++ lib/openchat/users/aggregates/user.ex
@@ -1,22 +1,26 @@
 defmodule Openchat.Users.Aggregates.User do
   defstruct [:id, :username, :password, :about]
   
+  alias __MODULE__, as: State
   alias Openchat.Users.Commands.AuthenticateUser
   alias Openchat.Users.Commands.RegisterUser
   alias Openchat.Users.Events.UserRegistered
 
-  def execute(%__MODULE__{username: username}, %RegisterUser{}) when username != nil, do:
+  def execute(%State{username: username}, %RegisterUser{}) when username != nil, do:
     {:error, :username_already_used}
-  def execute(%__MODULE__{}, %RegisterUser{} = command) do
+  def execute(%State{}, %RegisterUser{} = command) do
     %UserRegistered{id: UUID.uuid4(), username: command.username, password: command.password, about: command.about}
   end
 
-  def execute(%__MODULE__{password: password}, %AuthenticateUser{password: password}), do: :ok
-  def execute(%__MODULE__{}, %AuthenticateUser{}), do: {:error, :invalid_credentials}
+  def execute(
+    %State{username: username, password: password},
+    %AuthenticateUser{username: username, password: password}
+  ), do: :ok
+  def execute(%State{}, %AuthenticateUser{}), do: {:error, :invalid_credentials}
 
   # state mutators
 
-  def apply(%__MODULE__{} = state, %UserRegistered{} = event) do
+  def apply(%State{} = state, %UserRegistered{} = event) do
     %{state | id: event.id, username: event.username, password: event.password, about: event.about}
   end
 
