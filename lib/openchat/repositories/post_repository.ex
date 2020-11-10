1d543fe7afa4045fac6a0027688337dce6c6a483
Daniele Megna <daniele.megna@casavo.com>
2020-11-10 12:06:44 +0100

    Fix User and Post repositories behaviour spec
    store now do not returns the stored id

--- lib/openchat/repositories/post_repository.ex
+++ lib/openchat/repositories/post_repository.ex
@@ -1,5 +1,5 @@
 defmodule Openchat.Repositories.PostRepository do
   alias Openchat.Posts.Data.Post
   @callback get_by_userid(String.t) :: [Post.t]
-  @callback store(Post.t) :: String.t
+  @callback store(Post.t) :: :ok
 end
--- lib/openchat/repositories/user_repository.ex
+++ lib/openchat/repositories/user_repository.ex
@@ -3,5 +3,5 @@ defmodule Openchat.Repositories.UserRepository do
   @callback get_all() :: [User.t]
   @callback get_by_username(String.t) :: User.t
   @callback get_by_id(String.t) :: User.t
-  @callback store(User.t) :: String.t
+  @callback store(User.t) :: :ok
 end
